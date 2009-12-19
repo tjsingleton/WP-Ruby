49845b640e3a2a0510452d5e76a5de3dfd965def
TJ Singleton <tjsingleton@vantagestreet.com>
2009-12-18 21:32:38 -0500

    fixed typo

--- lib/wp/usermeta.rb
+++ lib/wp/usermeta.rb
@@ -2,7 +2,7 @@ class WP::Usermeta
   include DataMapper::Resource
   storage_names[:default] = "wp_usermeta"
 
-  property :umeta_id, Serial
+  property :meta_id, Serial
   property :user_id, Integer
   property :meta_key, String
   property :meta_value, Text
