d1934cd92dce42aa82688ba9a45294d975d8a675
TJ Singleton <tjsingleton@vantagestreet.com>
2009-12-23 01:47:00 -0500

    trying to get transactional testing working.

--- spec/models/wp_options_spec.rb
+++ spec/models/wp_options_spec.rb
@@ -1,4 +1,20 @@
 require 'spec/spec_helper'
 
 describe WP::Option do
+  before do
+    @option = WP::Option.first
+  end
+
+  it "updates" do
+    @option.option_value = "cool"
+    @option.save.should == true
+    @option.option_value.should == "cool"
+  end
+
+  it "resets" do
+    @option.reload
+    @option.value.should_not == "cool"
+    puts @option.value
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,20 @@ require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+    config.after(:each) do
+    repository(:default) do
+      while repository.adapter.current_transaction
+        repository.adapter.current_transaction.rollback
+        repository.adapter.pop_transaction
+      end
+    end
+  end
+
+  config.before(:each) do
+    repository(:default) do
+      transaction = DataMapper::Transaction.new(repository)
+      transaction.begin
+      repository.adapter.push_transaction(transaction)
+    end
+  end
 end
