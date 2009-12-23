19b70ebc173c7ff3d666a5723425515fb6a7d70f
TJ Singleton <tjsingleton@vantagestreet.com>
2009-12-23 01:27:06 -0500

    Revert "basic updates for mu"
    This reverts commit a114936b769d3308e53fccfeb726a3b470f477ea.

--- lib/wp/models/blog_version.rb
+++
@@ -1,7 +0,0 @@
-class WP::BlogVersion
-  include DataMapper::Resource
-
-  property :blog_id, Integer
-  property :db_version, String
-  property :last_updated, DateTime
-end
--- lib/wp/models/sitecategory.rb
+++
@@ -1,9 +0,0 @@
-class WP::Sitecategory
-  include DataMapper::Resource
-
-  property :cat_ID, Serial
-  property :cat_name, String
-  property :category_nicename, String
-  property :last_updated, EpochTime
-  
-end
