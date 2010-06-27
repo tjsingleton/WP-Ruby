4ced66fb2a30baaef9f1180bade1f6a8ca506bc5
TJ Singleton <tjsingleton@vantagestreet.com>
2010-06-27 03:26:27 -0400

    reorg

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,18 +5,19 @@ require 'wp-ruby'
 require 'rspec'
 require "mysql2"
 
-TABLES = %w[wp_commentmeta wp_comments wp_links wp_options wp_postmeta wp_posts wp_term_relationships wp_term_taxonomy wp_terms wp_usermeta wp_users]
-INITIAL_SQL = File.open("spec/inserts.sql").to_a
-
 MYSQL_CONNECTION = Mysql2::Client.new({
   :host => "localhost",
   :username => "root",
   :database => "wordpress_test"
 })
 
+
+TABLES = %w[wp_commentmeta wp_comments wp_links wp_options wp_postmeta wp_posts wp_term_relationships wp_term_taxonomy wp_terms wp_usermeta wp_users]
+INSERTS = File.open("spec/inserts.sql").to_a
+
 RSpec.configure do |config|
   config.before(:each) do
     TABLES.each {|table| MYSQL_CONNECTION.query("TRUNCATE TABLE #{table};") }
-    INITIAL_SQL.each {|line| MYSQL_CONNECTION.query(line) }
+    INSERTS.each {|line| MYSQL_CONNECTION.query(line) }
   end
 end
